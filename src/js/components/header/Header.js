0cdca7b099ee14107225cdf274dbd918f495ab6c
Princesso <egbunaoluebube@gmail.com>
2018-07-12 11:53:20 +0100

    create header component

---
+++ src/js/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from '../../../styles/header.css'
+
+const Header = () => {
+  return(
+    <div className="header">
+      <ul>
+        <li>Folk music</li>
+        <li>Folk tales</li>
+        <li>Folk art</li>
+        <li>Folk culture</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
