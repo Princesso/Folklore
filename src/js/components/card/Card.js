f7ca29a95c8745940461dc0fa05fdfce2e8df600
Princesso <egbunaoluebube@gmail.com>
2018-09-11 17:09:57 +0100

    set basic card makeup properties  for Card component

--- src/js/components/card/Card.js
+++ src/js/components/card/Card.js
@@ -2,7 +2,9 @@ import React from 'react'
 const Card = () => {
   return(
     <div className="header">
-      <
+      <img src="" alt=""/>
+      <h3>Name</h3>
+      <p>email</p>
     </div>
   )
 }
