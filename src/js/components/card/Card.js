fab8a94d142c091d421228c1ace56f1614e2cbb2
Princesso <egbunaoluebube@gmail.com>
2018-09-19 17:24:12 +0100

    create card component

--- src/js/components/card/Card.js
+++ src/js/components/card/Card.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledCard = styled.div `
+background : aquamarine ;
+height: 300px;
+width:200px;
+border-radius: 10px;;
+
+img{
+ heigtht: 200px;
+ width: 200px; 
+}
+`
+
 const Card = () => {
   return(
-    <div className="header">
-      <img src="" alt=""/>
+    <StyledCard>
+      <img src="https://robohash.org/princess" alt="Robot"/>
       <h3>Name</h3>
       <p>email</p>
-    </div>
+    </StyledCard>
   )
 }
 
