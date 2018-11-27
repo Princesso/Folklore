b29278865f88be3f9e1bdc5222b2aff9196cce4c
princesso <egbunaoluebube@gmail.com>
2018-11-27 13:52:44 +0100

    Make card to accept props

--- src/js/components/card/Card.js
+++ src/js/components/card/Card.js
@@ -5,7 +5,12 @@ const StyledCard = styled.div `
 background : aquamarine ;
 height: 300px;
 width:200px;
-border-radius: 10px;;
+border-radius: 10px;
+margin:10px;
+padding: 10px;
+:hover {
+  transform: scale(1.1)
+}
 
 img{
  heigtht: 200px;
@@ -13,12 +18,12 @@ img{
 }
 `
 
-const Card = () => {
+const Card = (props) => {
   return(
     <StyledCard>
       <img src="https://robohash.org/princess" alt="Robot"/>
-      <h3>Name</h3>
-      <p>email</p>
+      <h3>{props.name}</h3>
+      <p>{props.email}</p>
     </StyledCard>
   )
 }
