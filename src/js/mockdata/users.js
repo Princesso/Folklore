e4235175d2a6ff476e218a7d7c360cf3eca551e6
princesso <egbunaoluebube@gmail.com>
2018-11-27 13:51:09 +0100

    Create users mock data

---
+++ src/js/mockdata/users.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    id: 1,
+    name: "Adanne Oluoma",
+    username: "ada",
+    email: "adanne@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Oluebube Ulu",
+    username: "ulu",
+    email: "oluebube@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Amarachi Egb",
+    username: "amara",
+    email: "amara@gmail.com"
+  },
+  {
+    id: 4,
+    name: "Peculiar Ediomao",
+    username: "pediomo",
+    email: "pediomo@gmail.com"
+  }
+]
