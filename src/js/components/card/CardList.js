239f3e763edd32f0169ae88af15353d6bc7fc395
princesso <egbunaoluebube@gmail.com>
2018-11-27 17:12:04 +0100

    Dynamically update Card content

---
+++ src/js/components/card/CardList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from './Card'
+import styled from 'styled-components'
+
+const StyledCardList = styled.div`
+  
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content:center;
+  
+`
+
+const CardList = ({users}) => {
+  /**
+   * A map function returns each object in the array and optionally an index
+   */
+  const cardContainer = users.map((user,index) => {
+    return <Card id={users[index].id} name={users[index].name} username={users[index].username} email={users[index].email}/> 
+  })
+  return(
+    <StyledCardList>
+      {cardContainer}
+    </StyledCardList>
+  );
+}
+
+export default CardList;
