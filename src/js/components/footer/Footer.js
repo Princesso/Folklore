99900dad9c104e11f13ca624f68ebc1f2b7aa0bc
princesso <egbunaoluebube@gmail.com>
2018-12-07 13:19:54 +0100

    Create footer component

---
+++ src/js/components/footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+StyledFooter = styled.div`
+
+`
+
+const Footer = () => {
+  return(
+    <StyledFooter>
+      <ul>
+        <li>Copyright made by Oluebube </li>
+        <li>Contact</li>
+      </ul>
+    </StyledFooter>
+  )
+}
+
+export default Footer
