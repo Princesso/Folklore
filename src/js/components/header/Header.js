7f153ff12e420dfb31e759b74af8ab3da27f0fd4
princesso <egbunaoluebube@gmail.com>
2018-12-23 08:49:54 +0100

    Update header styles and text

--- src/js/components/header/Header.js
+++ src/js/components/header/Header.js
@@ -4,18 +4,20 @@ import styled from 'styled-components'
 const StyledHeader = styled.div `
     height:50px;
     width: 100%;
-    margin: 10px;
+    margin-bottom: 3%;
     padding: 10px;
 
     h2{
         text-align:center;
+        color: aquamarine ;
+        font-size: 3em;
     }
 `
 
 const Header =() =>{
     return(
         <StyledHeader>
-            <h2>Lorea Corporation</h2>
+            <h2>Friends Zone</h2>
         </StyledHeader>
     )
 }
