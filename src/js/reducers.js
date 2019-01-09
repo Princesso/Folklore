314d17eaf88e83c7ff8760e9a9e4082fcf12f95c
Oluebube Egbuna <oluebubeegbuna@Oluebubes-MacBook-Pro.local>
2019-01-09 15:12:52 +0100

    Add a reducer

---
+++ src/js/reducers.js
@@ -0,0 +1,12 @@
+import { CHANGE_SEARCH_FIELD } from './constants'
+
+const initialState = {
+    searchlist: ''
+}
+
+//Ensure that the name of the Reducer that is reducing the action represents the name of the action
+export const searchFriends = (state=initialState, action = {}) => {
+    switch(action.type){
+        
+    }
+}
