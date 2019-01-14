a608e3cd510395c5613fef070178196fb7188581
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-01-14 15:50:19 +0100

    Add switch statement to the reducer

--- src/js/reducers.js
+++ src/js/reducers.js
@@ -7,6 +7,9 @@ const initialState = {
 //Ensure that the name of the Reducer that is reducing the action represents the name of the action
 export const searchFriends = (state=initialState, action = {}) => {
     switch(action.type){
-        
+      case (CHANGE_SEARCH_FIELD):
+        return Object.assign({}, state, {searchFriends: action.payload})
+      default: 
+        return state
     }
 }
