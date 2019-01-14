89ac8ef5607d175451c95158a47276d0b2c196e9
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-01-14 18:17:33 +0100

    Remove create store component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider, connect} from 'react-redux'
-import {createStore} from 'redux'
-import {searchFriends} from './js/reducers'
+import store from './js/store'
 
-//create the store also import reducer functions, createStore, Provider and connect
 
-const store = createStore(searchFriends)
 ReactDOM.render(<App store= {store}/>, document.getElementById('root'));
 registerServiceWorker();
